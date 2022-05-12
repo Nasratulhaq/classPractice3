837038b58f6259eb64b4ae9683e2c1008c3200f9
Nasratulhaq <nasrtulhaq.fazli@tekschool.us>
2022-05-12 11:03:24 -0600

    this is the new class

---
+++ src/test.java
@@ -0,0 +1,10 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		System.out.println("This is the test");
+		System.out.println("This is the test");
+		System.out.println("This is the test");
+	}
+
+}
